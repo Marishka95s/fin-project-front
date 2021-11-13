90e8b7d33bf1dd9ed8fddc85d9419f81872be57e
SergeySidorchuk <serg_ss@ukr.net>
2021-11-13 22:59:41 +0200

    fixed bags(styles)

--- src/components/statistic/Chart/Chart.js
+++ src/components/statistic/Chart/Chart.js
@@ -2,46 +2,89 @@ import React from 'react';
 import './Chart.scss';
 import { Doughnut } from 'react-chartjs-2';
 
+// export default function Chart() {
+//   return (
+//     <div>
+//        <div className="chart">
+//         <Doughnut
+//           className="chart__contents"
+//           data={{
+//             datasets: [
+//               {
+//                 data: [20, 50, 100, 250, 500, 25, 36, 48, 1000],
+//                 backgroundColor: [
+//                   '#FED057',
+//                   '#FFD8D0',
+//                   '#FD9498',
+//                   '#C5BAFF',
+//                   '#6E78E8',
+//                   '#4A56E2',
+//                   '#81E1FF',
+//                   '#24CCA7',
+//                   '#00AD84',
+//                 ],
+//                 borderColor: [
+//                   '#FED057',
+//                   '#FFD8D0',
+//                   '#FD9498',
+//                   '#C5BAFF',
+//                   '#6E78E8',
+//                   '#4A56E2',
+//                   '#81E1FF',
+//                   '#24CCA7',
+//                   '#00AD84',
+//                 ],
+//                 borderWidth: 1,
+//                 hoverOffset: 4,
+//                 cutout: 99,
+//               },
+//             ],
+//           }}
+//         />
+//         <span className="chart__ballance">&#8372; {24000}</span>
+//        </div>
+//      </div>
+  
+//   );
+// }
+
 export default function Chart() {
+  const data = {
+    datasets: [
+      {
+       data: [20, 50, 100, 250, 500, 25, 36, 48, 1000],
+        backgroundColor: [
+          '#FED057',
+          '#FFD8D0',
+          '#FD9498',
+          '#C5BAFF',
+          '#6E78E8',
+          '#4A56E2',
+          '#81E1FF',
+          '#24CCA7',
+          '#00AD84',
+        ],
+        borderColor: [
+          '#FED057',
+          '#FFD8D0',
+          '#FD9498',
+          '#C5BAFF',
+          '#6E78E8',
+          '#4A56E2',
+          '#81E1FF',
+          '#24CCA7',
+          '#00AD84',
+        ],
+        borderWidth: 1,
+        cutout: 99,
+      },
+    ],
+  };
+
   return (
-    <div>
-      <div className="chart">
-        <Doughnut
-          className="chart__contents"
-          data={{
-            datasets: [
-              {
-                data: [20, 50, 100, 250, 500, 25, 36, 48, 1000],
-                backgroundColor: [
-                  '#FED057',
-                  '#FFD8D0',
-                  '#FD9498',
-                  '#C5BAFF',
-                  '#6E78E8',
-                  '#4A56E2',
-                  '#81E1FF',
-                  '#24CCA7',
-                  '#00AD84',
-                ],
-                borderColor: [
-                  '#FED057',
-                  '#FFD8D0',
-                  '#FD9498',
-                  '#C5BAFF',
-                  '#6E78E8',
-                  '#4A56E2',
-                  '#81E1FF',
-                  '#24CCA7',
-                  '#00AD84',
-                ],
-                borderWidth: 1,
-                hoverOffset: 4,
-              },
-            ],
-          }}
-        />
-        <span className="chart__ballance">&#8372; {24000}</span>
-      </div>
+    <div className="chart">
+      <Doughnut className="chart__contents" data={data} />
+      <span className="chart__ballance">&#8372; {250000}</span>
     </div>
   );
 }
