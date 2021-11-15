0781b95867adbf0d1d80d48eb0dd54293d2c8961
Yurii Khomin <yurii.khomin@gmail.com>
2021-11-15 18:07:52 +0200

    Добавил навигацию,спрайт иконок и функцию для их встраивания

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import logo from './wallet-logo.svg';
 import './App.scss';
+import Navigation from './components/dashboard/Navigation';
 import Balance from './components/dashboard/Balance';
 import { Routes, Route } from 'react-router';
 import RegistrationPage from './views/RegistrationPage/RegistrationPage';
@@ -9,12 +10,19 @@ import Diagram from '../src/components/statistic/Diagram';
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <Balance />
 
       <header className="App-header">
         <Routes>
-          <Route path="/fin-project-front/" element={<p>Допиши /registration или /login</p>} />
-          <Route path="/fin-project-front/registration" element={<RegistrationPage />} />
+          <Route
+            path="/fin-project-front/"
+            element={<p>Допиши /registration или /login</p>}
+          />
+          <Route
+            path="/fin-project-front/registration"
+            element={<RegistrationPage />}
+          />
           <Route path="/fin-project-front/login" element={<LoginPage />} />
         </Routes>
       </header>
