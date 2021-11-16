d0428c1973db283c5db73bdf6cf64d59c1ead437
Marishka95s <marishka95@ukr.net>
2021-11-16 19:57:46 +0200

    Routing in app

--- src/App.js
+++ src/App.js
@@ -1,52 +1,80 @@
 import logo from './wallet-logo.svg';
 
 import React, { Suspense, lazy, useEffect } from 'react';
-import { Switch } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { Switch, Redirect } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { authOperations, authSelectors } from './redux/auth';
+import Loader from 'react-loader-spinner';
 
 import PublicRoute from './components/PublicRoute'
+import PrivateRoute from './components/PrivateRoute'
 
 import './App.scss';
 import Currency from './components/currency/Currency';
 import Navigation from './components/dashboard/Navigation';
 import Balance from './components/dashboard/Balance/Balance';
 // import { Routes, Route } from 'react-router';
-import RegistrationPage from './views/RegistrationPage/RegistrationPage';
-import LoginPage from './views/LoginPage/LoginPage';
+// import RegistrationPage from './views/RegistrationPage/RegistrationPage';
+// import LoginPage from './views/LoginPage/LoginPage';
 import Diagram from '../src/components/statistic/Diagram';
 
 const HomePage = lazy(() => import('./views/HomePage/HomePage'))
+const StatisticPage = lazy(() => import('./views/StatisticPage/StatisticPage'))
+
+const RegistrationPage = lazy(() => import('./views/RegistrationPage/RegistrationPage' /* webpackChunkName: "RegisterView"*/),);
+const LoginPage = lazy(() => import('./views/LoginPage/LoginPage' /* webpackChunkName: "LoginView"*/),);
 
 export default function App() {
   const dispatch = useDispatch();
 
-    // useEffect(() => {
-    //     dispatch();
-    // }, [dispatch]);
+  const isFetchingCurrentUser = useSelector(authSelectors.getIsFetchingCurrentUser);
 
-  return( 
+  useEffect(() => {
+    dispatch(authOperations.fetchCurrentUser());
+  }, [dispatch]);
+
+  
+  return( !isFetchingCurrentUser &&(
     <div className="App">
-        <Suspense fallback={<p>Загружаем...</p>}>
+      <Suspense fallback={<Loader type="ThreeDots" color="brown" height={80} width={80} />}>
 
         <Switch>
-          {/* <PublicRoute exact path="/">
-          <HomePage />
-          </PublicRoute> */}
-          <HomePage />
+          <PublicRoute exact path="/registration" >
+            <RegistrationPage />
+          </PublicRoute>
+
+          <PublicRoute path="/login" restricted redirectTo="/login">
+            <LoginPage />
+          </PublicRoute>
+
+          <PrivateRoute path="/home" redirectTo="/home">
+            <HomePage />
+          </PrivateRoute>
+
+          <PrivateRoute path="/statistics" redirectTo="/statistics">
+            <StatisticPage />
+          </PrivateRoute>     
+
+          <PublicRoute>
+            <Redirect to="/registration" />
+          </PublicRoute>     
         </Switch>
+      </Suspense>
 
-        </Suspense>
+        
 
-      <Navigation />
-      <Diagram />
-      <Balance />
+          {/* <Navigation />
+          <Diagram />
+          <Balance /> 
+          <Currency/>
+          */}
 
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello) I'm a wallet and I believe in you!
         </p>
-        <Currency/>
+         */}
         {/* <Routes>
           <Route
             path="/fin-project-front/"
@@ -59,10 +87,11 @@ export default function App() {
           <Route path="/fin-project-front/login" element={<LoginPage />} />
           <Route path="/fin-project-front/statistics" element={<Diagram />} />
         </Routes> */}
-      </header>
+      {/* </header> */}
 
+      
     </div>
-  );
+  ));
 }
 // class App extends Component {
 //   state = {
--- src/components/authorization/RegistrationForm/RegistrationForm.js
+++ src/components/authorization/RegistrationForm/RegistrationForm.js
@@ -82,7 +82,7 @@ export default function RegistrationForm() {
                     <button className="login-button" type="submit">Регистрация</button>
                     <NavLink
                         className="nav-button"
-                        to="/fin-project-front/login"
+                        to="/login"
                         exact
                     >
                         Вход
