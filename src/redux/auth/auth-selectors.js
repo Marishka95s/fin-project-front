83d5c0f6b8e72657ae3de25709c6d1ebd005187b
Andrew <nonameznoone@gmail.com>
2021-11-18 21:07:16 +0200

    login fixed

--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -4,18 +4,10 @@ const getUserName = state => state.auth.user.name;
 
 const getUserEmail = state => state.auth.user.email;
 
-const getAuthError = state => state.auth.authError;
-
-const getRegError = state => state.auth.regError;
-
-const getIsFetchingCurrentUser = state => state.auth.isFetchingCurrentUser;
 
 const authSelectors = {
     getIsLoggedIn,
     getUserName,
     getUserEmail,
-    getIsFetchingCurrentUser,
-    getAuthError,
-    getRegError,
 };
 export default authSelectors;
