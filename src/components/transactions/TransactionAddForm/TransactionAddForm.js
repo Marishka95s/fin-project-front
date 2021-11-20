cc81302ffb96380a7454489c53bd1e2105176e5c
Marishka95s <marishka95@ukr.net>
2021-11-20 03:22:40 +0200

    optimization

--- src/components/transactions/TransactionAddForm/TransactionAddForm.js
+++ src/components/transactions/TransactionAddForm/TransactionAddForm.js
@@ -6,7 +6,6 @@ import Switch from 'react-switch';
 import Datetime from 'react-datetime';
 import moment from 'moment';
 import 'moment/locale/ru';
-import { closeModalTransaction } from "../../../redux/transactions/transactions-actions";
 import customStyles from './SelectStyles';
 import "react-datetime/css/react-datetime.css";
 import { transactionsOperations, transactionsSelectors } from "../../../redux/transactions";
@@ -15,9 +14,6 @@ import './TransactionAddForm.scss';
 
 export default function TransactionAddForm({ onClose }) {
     const dispatch = useDispatch();
-    // const onClose = useCallback(() => {
-    //     return dispatch(closeModalTransaction());
-    // }, [dispatch]);
 
     useEffect(() => {
         dispatch(transactionsOperations.getCategories());
@@ -39,7 +35,6 @@ export default function TransactionAddForm({ onClose }) {
     });
     
     const { type, sum, comment, category, checked, boxShadow } = fullState;
-    // const { value } = optionSelect;
     
     useEffect(() => {
         if (checked) {
@@ -69,7 +64,7 @@ export default function TransactionAddForm({ onClose }) {
       className: 'input date__input',
         };
         
-    let optionsIncome = [];
+    // let optionsIncome = [];
 
         //   const sort = array => {
         //     array.forEach(({ _id, name }) =>
@@ -85,10 +80,6 @@ export default function TransactionAddForm({ onClose }) {
         //     );
         //   };
         //   sort(categories);
-
-        
-        
-        
 
 
     const onChangeSelect = (e)=>{
@@ -193,7 +184,7 @@ export default function TransactionAddForm({ onClose }) {
                             offColor="#fff"
                             boxShadow={boxShadow}
                             checkedHandleIcon={
-                        <svg className="add-transaction__icon" id="add-icon" width="20" height="2" viewBox="0 0 20 2" fill="none" xmlns="http://www.w3.org/2000/svg">
+                             <svg className="add-transaction__icon" id="add-icon" width="20" height="2" viewBox="0 0 20 2" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M0 1L20 0.999999" stroke="white" stroke-width="2"/>
                         </svg>
                             }
@@ -302,144 +293,3 @@ export default function TransactionAddForm({ onClose }) {
             </Modal>
         )
     }
-
-
-// const modalRoot = document.querySelector('#modal-root');
-// class TransactionAddForm extends Component{
-//     state = {
-//         sum: null,
-//         type: "expense",
-//         category: null,
-//         data: null,
-//         comment: null,
-//         isModalAddTransactionOpen: false
-//     }
-//     componentDidMount() {
-//     window.addEventListener('keydown', this.handleKeyDown);
-//     }
-
-//     componentWillUnmount() {
-//     window.removeEventListener('keydown', this.handleKeyDown);
-//     }
-
-//     handleKeyDown = e => {
-//     if (e.code === 'Escape') {
-//         this.props.onClose();
-//     }
-//     };
-//     handleSubmil = event => {
-//         event.preventDefault();
-
-//         // this.props.onSubmit(this.state);
-
-//         this.reset();
-
-//         console.log(this.state);
-//     };
-
-//     handleChange = event => {
-//         const { name, value } = event.currentTarget;
-//         this.setState({ [name]: value });
-//     };
-
-//     reset = () => {
-//         this.setState({ sum: " ", type: "expense", category: " " , data: " ", comment: " " })
-//     };
-
-
-//     render() {
-//         const { sum, type, category ,data, comment } = this.state
-
-//         return createPortal(
-//             <div className="TransactionAddForm__backdrop">
-//                 <form className="TransactionAddForm__content" onSubmit={this.handleSubmil}>
-//                     <button type="button" className="TransactionAddForm__closeBtn">
-//                         <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-//                             <path d="M1 1L17 17" stroke="black"/>
-//                             <path d="M1 17L17 0.999999" stroke="black"/>
-//                         </svg>
-//                     </button>
-                    // <p className="TransactionAddForm__title">Добавить транзакцию</p>
-
-                    // <div className="TransactionAddForm__radio">
-                    //     <label className="TransactionAddForm__text">Доход
-                    //     <input
-                    //     type="radio"
-                    //     name="type"
-                    //     checked={type === "income"}
-                    //     value="income"
-                    //     onChange={this.handleChange}
-                    // />
-                    // </label>
-
-                    // <label className="TransactionAddForm__text">
-                    //     <input
-                    //     type="radio"
-                    //     checked={type === "expense"}
-                    //     name="type"
-                    //     value="expense"
-                    //     onChange={this.handleChange}
-                    // />Расход
-                    // </label>
-                    // </div>
-
-                    // <label>
-                    //     <input
-                    //         className="TransactionAddForm__input"
-                    //         type="text"
-                    //         name="category"
-                    //         value={category}
-                    //         onChange={this.handleChange}
-                    //         placeholder="Выберите категорию"
-                    //         required
-                    //     />
-                    // </label>
-
-                    // <div>
-                    //     <label htmlFor="">
-                    //         <input
-                    //             className="TransactionAddForm__input"
-                    //         type='number'
-                    //         name="sum"
-                    //         value={sum}
-                    //         onChange={this.handleChange}
-                    //         placeholder="0.00"
-                    //         required
-                    //     />
-                    //     </label>
-
-                    //     <label htmlFor="">
-                    //         <input
-                    //             className="TransactionAddForm__input"
-                    //         type='data'
-                    //         name="data"
-                    //         value={data}
-                    //         onChange={this.handleChange}
-                    //         placeholder="Выберите дату"
-                    //         required
-                    //     />
-                    // </label>
-                    // </div>
-
-                    // <label>
-                    //     <input
-                    //         className="TransactionAddForm__input"
-                    //         type='text'
-                    //         name="comment"
-                    //         value={comment}
-                    //         onChange={this.handleChange}
-                    //         placeholder="Комментарий"
-                    //         required
-                    //     />
-                    // </label>
-
-                    // <button type="submit" className="TransactionAddForm__addBtn">ДОБАВИТЬ</button>
-                    // <button type="submit" className="TransactionAddForm__cancelBtn">ОТМЕНА</button>
-
-//                 </form>
-//             </div>,
-//             modalRoot,
-//         )
-//     }
-// }
-// export default TransactionAddForm;
