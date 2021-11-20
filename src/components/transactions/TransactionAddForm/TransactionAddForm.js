0af72f2762d4d098a32aadb4aa46e883c4551131
R0st <dotarost@mail.ru>
2021-11-21 01:17:01 +0200

    correct toast and logout

--- src/components/transactions/TransactionAddForm/TransactionAddForm.js
+++ src/components/transactions/TransactionAddForm/TransactionAddForm.js
@@ -14,9 +14,6 @@ import {
 } from '../../../redux/transactions';
 import './TransactionAddForm.scss';
 
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
 export default function TransactionAddForm({ onClose }) {
   const dispatch = useDispatch();
 
@@ -134,16 +131,6 @@ export default function TransactionAddForm({ onClose }) {
           category,
         }),
       );
-      toast.error('🦄 Недостаточно средств на счету', {
-        position: 'top-right',
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: 'colored',
-      });
       onClose();
     },
     [type, comment, sum, category, onClose, dispatch],
@@ -170,7 +157,6 @@ export default function TransactionAddForm({ onClose }) {
   //     );
 
   return (
-    // <p>M O D A L</p>
 
     <Modal onClose={onClose}>
       <button
@@ -275,20 +261,9 @@ export default function TransactionAddForm({ onClose }) {
               placeholder="Выберите категорию"
               styles={customStyles}
             />
-            <svg
-              className="select__icon"
-              id="arrow-icon"
-              width="20"
-              height="11"
-              viewBox="0 0 20 11"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M1 1L10 10L19 1" stroke="black" />
-            </svg>
+            {/* <Icons className="select__icon" id="arrow-icon" /> */}
           </div>
         )}
-
         <div className="form__wrapper">
           <label className="form__sum">
             <input
@@ -315,59 +290,23 @@ export default function TransactionAddForm({ onClose }) {
             // onChange={handleChangeDate}
             // required
           />
-          <svg
-            className="date__icon"
-            id="calendar-icon"
-            width="26"
-            height="30"
-            viewBox="0 0 26 30"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <g filter="url(#filter0_d_4_1061)">
-              <path
-                d="M10 11H8V13H10V11ZM14 11H12V13H14V11ZM18 11H16V13H18V11ZM20 4H19V2H17V4H9V2H7V4H6C4.89 4 4.01 4.9 4.01 6L4 20C4 21.1 4.89 22 6 22H20C21.1 22 22 21.1 22 20V6C22 4.9 21.1 4 20 4ZM20 20H6V9H20V20Z"
-                fill="#4A56E2"
-              />
-            </g>
-            <defs>
-              <filter
-                id="filter0_d_4_1061"
-                x="-3"
-                y="0"
-                width="32"
-                height="32"
-                filterUnits="userSpaceOnUse"
-                color-interpolation-filters="sRGB"
-              >
-                <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                <feColorMatrix
-                  in="SourceAlpha"
-                  type="matrix"
-                  values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                  result="hardAlpha"
-                />
-                <feOffset dy="4" />
-                <feGaussianBlur stdDeviation="2" />
-                <feComposite in2="hardAlpha" operator="out" />
-                <feColorMatrix
-                  type="matrix"
-                  values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
-                />
-                <feBlend
-                  mode="normal"
-                  in2="BackgroundImageFix"
-                  result="effect1_dropShadow_4_1061"
-                />
-                <feBlend
-                  mode="normal"
-                  in="SourceGraphic"
-                  in2="effect1_dropShadow_4_1061"
-                  result="shape"
-                />
-              </filter>
-            </defs>
-          </svg>
+          {/* <svg className="date__icon" id="calendar-icon" width="26" height="30" viewBox="0 0 26 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g filter="url(#filter0_d_4_1061)">
+<path d="M10 11H8V13H10V11ZM14 11H12V13H14V11ZM18 11H16V13H18V11ZM20 4H19V2H17V4H9V2H7V4H6C4.89 4 4.01 4.9 4.01 6L4 20C4 21.1 4.89 22 6 22H20C21.1 22 22 21.1 22 20V6C22 4.9 21.1 4 20 4ZM20 20H6V9H20V20Z" fill="#4A56E2"/>
+</g>
+<defs>
+<filter id="filter0_d_4_1061" x="-3" y="0" width="32" height="32" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset dy="4"/>
+<feGaussianBlur stdDeviation="2"/>
+<feComposite in2="hardAlpha" operator="out"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_4_1061"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_4_1061" result="shape"/>
+</filter>
+</defs>
+</svg> */}
         </div>
 
         <label className="form__text">
