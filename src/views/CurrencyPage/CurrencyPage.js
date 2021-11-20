c4a9d7225d998762e1de91ac970bfa185c8404d7
Marishka95s <marishka95@ukr.net>
2021-11-20 23:33:37 +0200

    redirect from mobile currency

--- src/views/CurrencyPage/CurrencyPage.js
+++ src/views/CurrencyPage/CurrencyPage.js
@@ -1,34 +1,56 @@
 import Media from 'react-media';
-
+import React, { Suspense} from 'react';
+import { Redirect } from 'react-router-dom';
 import AuthBar from '../../components/authorization/AuthBar/AuthBar';
 import Currency from '../../components/currency/Currency';
 import NavigationBtns from '../../components/dashboard/Navigation';
-import Balance from '../../components/dashboard/Balance/Balance';
-import Diagram from '../../components/statistic/Diagram';
+import Loader from 'react-loader-spinner';
+
+import PrivateRoute from '../../components/PrivateRoute'
 
 import './CurrencyPage.scss';
 
-
 export default function CurrencyPage() {
-    return (
-        <>
-            <Media query="(max-width: 767px)" render={() =>
-            (<> <AuthBar />
+  return (
+    <>
+      <Media
+        query="(max-width: 767px)"
+        render={() => (
+          <>
+            {' '}
+            <AuthBar />
             <div className="statisticsPage">
-                <div className="left-side-block" >                    
-                    <NavigationBtns />                    
+              <div className="left-side-block">
+                <NavigationBtns />
+              </div>
+              <div className="right-side-block">
+                <div
+                  style={{
+                    width: '334px',
+                    display: 'inline-flex',
+                    'justify-content': 'center',
+                    'margin-top': '30px',
+                  }}
+                >
+                  <Currency />
                 </div>
-                <div className="right-side-block" > 
-                <div style={{"width" : "334px", "display": "inline-flex", "justify-content": "center", "margin-top": "30px"}}>                  
-                    <Currency/>
-                </div> 
-                </div>
+              </div>
             </div>
-            </>
-            )}
-            />            
-        </>
-
-
-    );
-};
+          </>
+        )}
+      />
+      <Media
+        query="(min-width: 768px)"
+        render={() => (
+          <>
+            <Suspense fallback={<Loader type="ThreeDots" color="brown" height={80} width={80} />}>                
+                <PrivateRoute path="/fin-project-front/currency" redirectTo="/fin-project-front/home" >
+                    <Redirect to="/fin-project-front/home" />
+                </PrivateRoute>
+            </Suspense>
+          </>
+        )}
+      />
+    </>
+  );
+}
