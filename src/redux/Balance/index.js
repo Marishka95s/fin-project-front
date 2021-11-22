1309ea703625ab3d04dd6b2072300a89adcbc937
Ievgen-Rybalko <jackrjack1@gmail.com>
2021-11-22 04:16:30 +0200

    upd: Balance was connected to store; known-issue: token not added to axios at once

--- src/redux/Balance/index.js
+++ src/redux/Balance/index.js
@@ -1,3 +1,4 @@
-// export * from './balance-selectors';
-// export * from './balance-actions';
-// export { default as balanceReducer } from './balance-reducer';
+export * as balanceActions from './balance-actions';
+export * as balanceOperations from './balance-operations';
+export { default as CombineReducers } from './balance-reducer';
+export * as balanceSelectors from './balance-selectors';
