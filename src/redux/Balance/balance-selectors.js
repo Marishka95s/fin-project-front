4a4cef6d5f810d538df11ed18912f98f6f87b990
Marishka95s <marishka95@ukr.net>
2021-12-09 16:16:55 +0200

    without trubled balance

--- src/redux/Balance/balance-selectors.js
+++
@@ -1,5 +0,0 @@
-const getBalance = state => state.balance.balance;
-const getBalanceIsLoading = state => state.balance.balanceIsLoading;
-const getBalanceError = state => state.balance.balanceError;
-
-export { getBalance, getBalanceIsLoading, getBalanceError };
--- src/redux/Balance/index.js
+++
@@ -1,4 +0,0 @@
-export * as balanceActions from './balance-actions';
-export * as balanceOperations from './balance-operations';
-export { default as CombineReducers } from './balance-reducer';
-export * as balanceSelectors from './balance-selectors';
